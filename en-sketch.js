d2d3403e7926e30287e6f6f65ee1c6a93d096168
Charitssa Stone <47705696+stxne@users.noreply.github.com>
2025-04-24 14:35:49 -0400

    Update en-sketch.js

--- en-sketch.js
+++ en-sketch.js
@@ -23,10 +23,9 @@ function setup() {
 
 function draw() {
 
-  while(images.length > 0) {
-    let currentImage = images[imageIndex]; // Use modulo to loop
+  for (let i = 0; i < images.length; i ++) {
+    let currentImage = images[i]; // Use modulo to loop
     image(currentImage, random(100,windowWidth),random(100,windowHeight), 200,275);
-    imageIndex++; // Move to the next image for the next click
   }
   
 }
