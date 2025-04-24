d9c4da1766363667f54d203526513ad1b770c624
Charitssa Stone <47705696+stxne@users.noreply.github.com>
2025-04-24 14:23:13 -0400

    Update en-sketch.js

--- en-sketch.js
+++ en-sketch.js
@@ -19,7 +19,7 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   if (images.length > 0) {
-    let currentImage = images[imageIndex % images.length]; // Use modulo to loop
+    let currentImage = images[imageIndex]; // Use modulo to loop
     image(currentImage, random(100,windowWidth),random(100,windowHeight), 200,275);
     imageIndex++; // Move to the next image for the next click
   }
