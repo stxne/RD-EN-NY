fd6dd6f2745db0c6dcd0e8814cf8a3a94fd77154
Charitssa Stone <47705696+stxne@users.noreply.github.com>
2025-04-24 11:52:00 -0400

    updated

--- sketch.js
+++ sketch.js
@@ -53,56 +53,7 @@ function preload() {
   images[47] = loadImage('images/img20250420_02530938_48.jpg');
   images[48] = loadImage('images/img20250420_02530938_49.jpg');
   images[49] = loadImage('images/img20250420_02530938_50.jpg');
-  images[50] = loadImage('images/img20250420_02530938_51.jpg');
-  images[51] = loadImage('images/img20250420_02530938_52.jpg');
-  images[52] = loadImage('images/img20250420_02530938_53.jpg');
-  images[53] = loadImage('images/img20250420_02530938_54.jpg');
-  images[54] = loadImage('images/img20250420_02530938_55.jpg');
-  images[55] = loadImage('images/img20250420_02530938_56.jpg');
-  images[56] = loadImage('images/img20250420_02530938_57.jpg');
-  images[57] = loadImage('images/img20250420_02530938_58.jpg');
-  images[58] = loadImage('images/img20250420_02530938_59.jpg');
-  images[59] = loadImage('images/img20250420_02530938_60.jpg');
-  images[60] = loadImage('images/img20250420_02530938_61.jpg');
-  images[61] = loadImage('images/img20250420_02530938_62.jpg');
-  images[62] = loadImage('images/img20250420_02530938_63.jpg');
-  images[63] = loadImage('images/img20250420_02530938_64.jpg');
-  images[64] = loadImage('images/img20250420_02530938_65.jpg');
-  images[65] = loadImage('images/img20250420_02530938_66.jpg');
-  images[66] = loadImage('images/img20250420_02530938_67.jpg');
-  images[67] = loadImage('images/img20250420_02530938_68.jpg');
-  images[68] = loadImage('images/img20250420_02530938_69.jpg');
-  images[69] = loadImage('images/img20250420_02530938_70.jpg');
-  images[70] = loadImage('images/img20250420_02530938_71.jpg');
-  images[71] = loadImage('images/img20250420_02530938_72.jpg');
-  images[72] = loadImage('images/img20250420_02530938_73.jpg');
-  images[73] = loadImage('images/img20250420_02530938_74.jpg');
-  images[74] = loadImage('images/img20250420_02530938_75.jpg');
-  images[75] = loadImage('images/img20250420_02530938_76.jpg');
-  images[76] = loadImage('images/img20250420_02530938_77.jpg');
-  images[77] = loadImage('images/img20250420_02530938_78.jpg');
-  images[78] = loadImage('images/img20250420_02530938_79.jpg');
-  images[79] = loadImage('images/img20250420_02530938_80.jpg');
-  images[80] = loadImage('images/img20250420_02530938_81.jpg');
-  images[81] = loadImage('images/img20250420_02530938_82.jpg');
-  images[82] = loadImage('images/img20250420_02530938_83.jpg');
-  images[83] = loadImage('images/img20250420_02530938_84.jpg');
-  images[84] = loadImage('images/img20250420_02530938_85.jpg');
-  images[85] = loadImage('images/img20250420_02530938_86.jpg');
-  images[86] = loadImage('images/img20250420_02530938_87.jpg');
-  images[87] = loadImage('images/img20250420_02530938_88.jpg');
-  images[88] = loadImage('images/img20250420_02530938_89.jpg');
-  images[89] = loadImage('images/img20250420_02530938_90.jpg');
-  images[90] = loadImage('images/img20250420_02530938_91.jpg');
-  images[91] = loadImage('images/img20250420_02530938_92.jpg');
-  images[92] = loadImage('images/img20250420_02530938_93.jpg');
-  images[93] = loadImage('images/img20250420_02530938_94.jpg');
-  images[94] = loadImage('images/img20250420_02530938_95.jpg');
-  images[95] = loadImage('images/img20250420_02530938_96.jpg');
-  images[96] = loadImage('images/img20250420_02530938_97.jpg');
-  images[97] = loadImage('images/img20250420_02530938_98.jpg');
-  images[98] = loadImage('images/img20250420_02530938_99.jpg');
-  images[99] = loadImage('images/img20250420_02530938_100s.jpg');
+  
   // Add more loadImage calls in the order you want them to appear
 }
 
