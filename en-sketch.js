7ebd2d319050b158ffd9a334f0233fbba4e2ce63
Charitssa Stone <47705696+stxne@users.noreply.github.com>
2025-04-24 15:56:39 -0400

    Update en-sketch.js

--- en-sketch.js
+++ en-sketch.js
@@ -1,17 +1,10 @@
 let images = [];
-let poem =[
-  "The Heights, a microcosm of the American Dream",
-  "The nucleus of Dominicanidad in New York",
-  "We find home in these tiendas", 
-  "We seek solace in frio frios and pastelitos.",
-  "This land is our flesh and bone",
-  "our home away from home on Broadway and it shows…", 
-  "- WASHINGTON HEIGHTS BY DILSON HERNANDEZ"
-
-];
+let poem = "The Heights, a microcosm of the American Dream\nThe nucleus of Dominicanidad in New York\nWe find home in these tiendas\nWe seek solace in frio frios and pastelitos.\nThis land is our flesh and bone\nour home away from home on Broadway and it shows…\n- WASHINGTON HEIGHTS BY DILSON HERNANDEZ";
+let bbox;
 
 function preload() {
   // Load your images here in the desired order.
+  font = loadFont('FONTS/guida-mono-sharp-regular-pro.woff2');
   images[0] = loadImage('images/img20250210_02062555.jpg');
   images[1] = loadImage('images/img20250210_02293431.jpg');
   images[2] = loadImage('images/img20250210_02425114.jpg');
@@ -27,12 +20,27 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight); 
+
+  textFont(font);
+  textSize(15);
+  textAlign(CENTER, CENTER);
+  
+  fill(0);
+  bbox = font.textBounds(poem, mouseX, MouseY);
+  rect(bbox.x, bbox.y, bbox.w, bbox.h);
+  
+  fill(255);
+  text(poem, mouseX, mouseY);
+
   for (let i = 0; i < images.length; i ++) {
-    image(images[i], random(0,windowWidth),random(100,windowHeight), 200,275);
+     image(images[i], random(10,x-10),random(10,y-10), 200,275);
+     
   }
 }
 
 function draw() {
+  
+  
 
   
   
